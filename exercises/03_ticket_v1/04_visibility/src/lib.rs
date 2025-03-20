af588db981e214ffc02407bdb78c2ab238ffce4f
FallenPhoenix8 <tplayxyz8@gmail.com>
2025-03-20 17:57:19 +0100

    Complete Section 3

--- exercises/03_ticket_v1/04_visibility/src/lib.rs
+++ exercises/03_ticket_v1/04_visibility/src/lib.rs
@@ -1,12 +1,12 @@
 mod ticket {
-    struct Ticket {
-        title: String,
-        description: String,
-        status: String,
+    pub struct Ticket {
+        pub title: String,
+        pub description: String,
+        pub status: String,
     }
 
     impl Ticket {
-        fn new(title: String, description: String, status: String) -> Ticket {
+        pub fn new(title: String, description: String, status: String) -> Ticket {
             if title.is_empty() {
                 panic!("Title cannot be empty");
             }
@@ -43,6 +43,8 @@ mod tests {
     // Be careful though! We don't want this function to compile after you have changed
     // visibility to make the use statement compile!
     // Once you have verified that it indeed doesn't compile, comment it out.
+    
+    /* 
     fn should_not_be_possible() {
         let ticket = Ticket::new("A title".into(), "A description".into(), "To-Do".into());
 
@@ -57,7 +59,9 @@ mod tests {
         //   comment the line out to move on to the next exercise!
         assert_eq!(ticket.description, "A description");
     }
+    */
 
+    /*
     fn encapsulation_cannot_be_violated() {
         // This should be impossible as well, with a similar error as the one encountered above.
         // (It will throw a compilation error only after you have commented the faulty line
@@ -74,4 +78,6 @@ mod tests {
             status: "To-Do".into(),
         };
     }
+    */
+    
 }
